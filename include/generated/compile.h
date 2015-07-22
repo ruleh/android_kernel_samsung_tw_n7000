ad3a4bb85bc3a3d374b09d8c9dcfa6e54075b522
ruleh <ruleh@users.noreply.github.com>
2015-07-22 17:40:22 +0200

    other stuff

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 22 17:20:55 CEST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "FoFH"
+#define LINUX_COMPILER "gcc version 4.8.5 20150515 (prerelease) (UBERTC-4.8.5) "
